ecb6e3d3eb367f2ccbbb95c4e779f822aac7ca39
gavin1818 <gavin1818@hotmail.com>
2017-04-11 20:26:05 -0700

    add strcture

---
+++ HangmanManager.java
@@ -0,0 +1,20 @@
+public class HangmanManager{
+	public HangmanManager(List<String> dictionary, int length, int max){
+
+	}
+	public Set<String> words(){
+
+	}
+	public int guessesLeft(){
+
+	}
+	public Set<Character> guesses(){
+
+	}
+	public String pattern(){
+
+	}
+	public int record(char guess){
+		
+	}
+}
